f4281e1164464707a05a3b64280367e3a3723e9f
heltnx <helenetnx@yahoo.fr>
2024-01-12 17:20:54 +0100

    ajustements

--- src/pages/realisations/realisations.js
+++ src/pages/realisations/realisations.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Layout from '../../layouts/Layout';
-import './realisations.css'
+import './realisations.css';
 import VideoWrapper from '../../layouts/video-wraper/VideoWrapper.jsx';
 import video1 from '../../assets/videos/gabari-tab.mp4';
 import video2 from '../../assets/videos/gabari-ordi.mp4';
 import video3 from '../../assets/videos/gabari-mobile.mp4';
-import video4 from '../../assets/videos/ohmyfood.mp4'
-import image5 from '../../assets/images/ohmyfood.jpg'
-
+import video4 from '../../assets/videos/ohmyfood.mp4';
+import image5 from '../../assets/images/ohmyfood.jpg';
+import image6 from '../../assets/images/ohmyfood-sass-git.jpg';
+import image7 from '../../assets/images/caroussel.jpg'
 const Realisations = () => {
 
     const videosSection1 = [video1, video2, video3];
-    const videosSection2 = [image5, video4];
+    const videosSection2 = [<img src={image5} alt="Project 5" />, video4, <img src={image6} alt="Project 6" />];
+    const videosSection3 = [<img src={image7} alt="Project 5" />];
 
     return (
         <Layout >
@@ -20,8 +22,7 @@ const Realisations = () => {
                 <div className="section_container">
 
                     <h2>Création de la page d'accueil d'une agence de voyage avec <nobr> <b>HTML</b> &amp; <b>CSS</b></nobr></h2>
-
-                    <VideoWrapper videos={videosSection1} />
+                    <VideoWrapper mediaList={videosSection1} />
 
                     <div >
                         <p>
@@ -30,15 +31,14 @@ const Realisations = () => {
                         </p>
                     </div>
                 </div>
-                <div className="section_container">
 
+                <div className="section_container">
                     <h2>Améliorez l'interface d'un site mobile avec des animations <b>CSS</b></h2>
 
-                    <VideoWrapper videos={videosSection2} />
+                    <VideoWrapper mediaList={videosSection2} />
 
-                    <div className='container'>
-                        <p>
-                            Intégrer une <strong>maquette en mobile-first</strong></p>
+                    <div >
+                        <p>Intégrer une <strong>maquette en mobile-first</strong></p>
                         <p>Mettre en œuvre des animations <strong>CSS</strong></p>
                         <p>Versionner son projet avec <strong><nobr>Git et Github</nobr></strong><br></br>
                             utiliser<strong> Sass </strong>.
@@ -46,6 +46,15 @@ const Realisations = () => {
                     </div>
                 </div>
 
+                <div className="section_container">
+                    <h2>Premiers pas sur le langage JavaScript <b>JavaScript</b></h2>
+
+                    <VideoWrapper mediaList={videosSection3} className="image-alone" />
+
+                    <div >
+                        <p>Programmer en utilisant les fondamentaux de <strong>JavaScript</strong></p>
+                    </div>
+                </div>
             </div>
         </Layout >
     );
